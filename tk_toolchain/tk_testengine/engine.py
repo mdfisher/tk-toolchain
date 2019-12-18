acc2f6b0bcc4d5213b816b8a44e26f6c64b98afd
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-12-18 12:13:57 -0500

    documented everything

--- tk_toolchain/tk_testengine/engine.py
+++ tk_toolchain/tk_testengine/engine.py
@@ -46,29 +46,57 @@ class TestEngine(sgtk.platform.Engine):
         self.__build_menu(self.context)
 
     def __build_menu(self, ctx):
+        """
+        Build the DCC menu. No-op of the engine doesn't implement the menu
+        building interface.
+
+        :param ctx: Current context.
+        """
+
+        # Copy the list of commands since we'll be doing edits to it.
+        commands = self.commands.copy()
+
+        # Extract favorites and context menu items from the list of commands
+        # because they each go to a dedicated section.
+        favorites = self._extract_favorites(commands)
+        context_menu_items = self._extract_context_menu_items(commands)
+
+        # Let the DCC do some further filtering. Certain DCCs, like Nuke, will add
+        # shortcuts to apps in other sections of the UI.
         commands = getattr(None, "filter_menu_commands", lambda cmds, ctx: cmds)(
             self.commands, ctx
         )
 
-        favorites, context_menu_commands = self._split_commands(commands)
-
-        menus = [self._generate_context_menu(ctx, context_menu_commands)]
+        # Generates the first menu entry, the context menu.
+        menus = [self._generate_context_menu(ctx, context_menu_items)]
         self._add_separator(menus)
 
+        # Then follows all the favorites, if any.
         if favorites:
             self._add_separator(menus)
             menus.extend(favorites)
             self._add_separator(menus)
 
+        # Finally we add the actions.
         self._add_menu_actions(menus, commands)
 
+        # builds the menu
         self._build_menu(menus)
 
     def _add_menu_actions(self, menu, commands):
+        """
+        Add menu actions to a given menu.
 
+        :param list menu: List of menu items to add to.
+        :param commands: ``dict`` of commands to be added. See :meth:`Engine.commands` for more information.
+        """
         commands_per_app = {}
 
+        # First, we need to group commands per application so we can create a sub-menu
+        # for each.
         for name, details in commands.items():
+            # Only apps show up under their name. Framework commands show up as Other Items.
+            # FIXME: Why??? This was standard practice in the engines but documented nowhere.
             app = details["properties"].get("app")
             try:
                 app_name = app.display_name
@@ -76,29 +104,38 @@ class TestEngine(sgtk.platform.Engine):
                 app_name = "Other Items"
             commands_per_app.setdefault(app_name, {})[name] = details
 
+        # For each app
         for app_name in sorted(commands_per_app):
+            # If there's only a single command for that app, create a single menu item.
             if len(commands_per_app[app_name]) == 1:
                 name, details = list(commands_per_app[app_name].items()).pop()
                 self._add_command_to_menu(menu, name, details)
             else:
+                # If there's more than one, group them under a menu named after
+                # the app name.
                 app_menu = {"type": "menu", "title": app_name, "children": []}
                 menu.append(app_menu)
                 for name, details in commands_per_app[app_name].items():
-                    self._add_command_to_menu(menu, name, details)
-
-    def _split_commands(self, commands):
-        favorites = self._extract_favorites(commands)
-        context_menu_items = self._extract_context_menu_items(commands)
-
-        return favorites, context_menu_items
+                    self._add_command_to_menu(app_menu["children"], name, details)
 
     def _extract_favorites(self, commands):
+        """
+        Extract favorites from the list of commands.
+
+        :param commands: ``dict`` of commands. See :meth:`Engine.commands` for more information.
+
+        :returns: List of menu items to create.
+        :rtype: list
+        """
         favorites = []
+
         for fav in self.get_setting("menu_favourites", []):
             app_instance_name = fav["app_instance"]
             menu_name = fav["name"]
 
+            # For each of the favorite in the list of commands.
             for name, details in list(commands.items()):
+                # If the favorite matches the current app.
                 if (
                     self._get_app_instance_name(details) == app_instance_name
                     and name == menu_name
@@ -107,6 +144,7 @@ class TestEngine(sgtk.platform.Engine):
                     del commands[name]
                     break
             else:
+                # No match was made, log a warning.
                 self.logger.warning(
                     "Could not add favorite app %s:%s", app_instance_name, menu_name
                 )
@@ -114,7 +152,17 @@ class TestEngine(sgtk.platform.Engine):
         return favorites
 
     def _extract_context_menu_items(self, commands):
+        """
+        Extract context menu items from the list of commands.
+
+        :param commands: ``dict`` of commands. See :meth:`Engine.commands` for more information.
+
+        :returns: List of menu items to create.
+        :rtype: list
+        """
         context_menu_items = []
+
+        #
         for name in sorted(commands):
             details = commands[name]
             if details["properties"].get("type") == "context_menu":
@@ -124,6 +172,14 @@ class TestEngine(sgtk.platform.Engine):
         return context_menu_items
 
     def _get_app_instance_name(self, details):
+        """
+        Get the app instance name for a given application details.
+
+        :param details: Details object taken from :meth:`Engine.commands`.
+
+        :returns: Name of the application instance or ``None`` if the application
+            instance was not found.
+        """
         app = details["properties"].get("app")
         if app is None:
             return None
@@ -134,22 +190,14 @@ class TestEngine(sgtk.platform.Engine):
 
         return None
 
-    def _build_menu(self, menu_items, parent_menu=None):
-        if parent_menu is None:
-            parent_menu = self._menu_bar.addMenu("Shotgun")
-
-        for item in menu_items:
-
-            if item["type"] == "separator":
-                parent_menu.addSeparator()
-            elif item["type"] == "menu":
-                print(item)
-                sub_menu = parent_menu.addMenu(item["title"])
-                self._build_menu(item["children"], sub_menu)
-            else:
-                parent_menu.addAction(item["title"]).triggered.connect(item["callback"])
-
     def _add_command_to_menu(self, menu, name, details):
+        """
+        Add a command to the menu.
+
+        :param menu: List of children to add to.
+        :param name: Name of the command.
+        :param details: Details for a command.
+        """
         menu.append(
             self._create_menu_item(
                 name, details["callback"], details["properties"].get("icon")
@@ -157,9 +205,22 @@ class TestEngine(sgtk.platform.Engine):
         )
 
     def _create_menu_item(self, title, callback, icon=None):
+        """
+        Create a menu item ``dict``.
+
+        :param title: Title of the menu.
+        :param callback: Callable to invoke when the menu is selected.
+        :param icon: Icon to display next to the menu item.
+        """
         return {"type": "action", "title": title, "icon": icon, "callback": callback}
 
-    def _generate_context_menu(self, ctx, context_menu_items):
+    def _generate_context_menu(self, ctx, context_menu_commands):
+        """
+        Create the context menu.
+
+        :param ctx: Current context.
+        :param context_menu_commands: Commands to add to the context menu.
+        """
         items = []
         context_menu = {"type": "menu", "title": str(ctx), "children": items}
 
@@ -169,10 +230,15 @@ class TestEngine(sgtk.platform.Engine):
                 self._create_menu_item("Jump to File System", self._jump_to_fs)
             )
         self._add_separator(items)
-        items.extend(context_menu_items)
+        items.extend(context_menu_commands)
         return context_menu
 
     def _add_separator(self, menu):
+        """
+        Add a separator to the menu.
+
+        :param list: List of menu items.
+        """
         menu.append({"type": "separator"})
 
     def _jump_to_sg(self):
@@ -194,6 +260,25 @@ class TestEngine(sgtk.platform.Engine):
                 "file://{0}".format(disk_location).replace("\\", "/")
             ):
                 self.logging.error("Failed to open '%s'!", disk_location)
+
+    def _build_menu(self, menu_items, parent_menu=None):
+        """
+        DCC
+        """
+        if parent_menu is None:
+            parent_menu = self._menu_bar.addMenu("Shotgun")
+
+        for item in menu_items:
+            if item["type"] == "separator":
+                parent_menu.addSeparator()
+            elif item["type"] == "menu":
+                sub_menu = parent_menu.addMenu(item["title"])
+                self._build_menu(item["children"], sub_menu)
+            else:
+                action = parent_menu.addAction(item["title"])
+                action.triggered.connect(item["callback"])
+                if item["icon"]:
+                    action.setIcon(sgtk.platform.qt.QtGui.QIcon(item["icon"]))
 
     @property
     def q_app(self):
